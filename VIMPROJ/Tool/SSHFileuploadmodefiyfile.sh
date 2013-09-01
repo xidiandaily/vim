d7360e9f175e720a348e39965b21b8db64c190fa
chiyl <xidiandaily@126.com>
2013-09-01 17:46:24 +0800

    Chg:1.一键上传功能支持端口配置;
    Signed-off-by: chiyl <xidiandaily@126.com>

--- VIMPROJ/Tool/SSHFileuploadmodefiyfile.sh
+++ VIMPROJ/Tool/SSHFileuploadmodefiyfile.sh
@@ -1,37 +1,75 @@
 #!/bin/sh
 
+if [ $# -ne 4 ];then
+	echo "argumemnt number err";
+	read
+	exit 0;
+fi
+
 export PATH=/usr/local/bin:/usr/bin:$PATH
 WorkPath=`pwd`;
 DIR=$WorkPath/$1;
 ROMOTE=$2;
-CHOICE=$3;
+PORT=$3;
+CHOICE=$4;
 
+shell_type=`uname`;
+
+if  [[ $shell_type =~ "CYGWIN" ]];then
+	echo "oh~, Cygwin"
+
+	if [ ! -d $DIR ];then
+		echo "director "$DIR" not exist!!";
+		read
+		exit 0;
+	fi
+
+
+else 
+	if [ ! -d $DIR ];then
+		echo "director "$DIR" not exist!!";
+		read
+		exit 0;
+	fi
+
+	if ! [[ $ROMOTE =~ '^.*[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\:.*$' ]];then
+		echo "remote ip( $ROMOTE ) is not support!!";
+		read
+		exit 0;
+	fi
+
+	if ! [[ $PORT =~ '^[0-9]+$' ]];then
+		echo "PORT: $PORT is not a support port!!";
+		read
+		exit 0;
+	fi
+
+fi
 
 TIMESTEMP=timestamp.txt
 FILELIST=uploadfile.txt
 
 cd $DIR;
 count=0;
-if [ $CHOICE -eq "2" ];then
+if [ "$CHOICE" -eq 2 ];then
 	rm $TIMESTEMP;
 fi
 if [ -f "$TIMESTEMP" ];then
-	#for i in `find -name "*[cpp|h|Makefile]" -newer $TIMESTEMP -type f `; do 
-	for i in `find -regextype awk  -regex ".*(cpp|h|Makefile)$" -newer $TIMESTEMP `; do
+	for i in `find . -newer $TIMESTEMP -type f `; do 
 		n=`echo $i|sed -e "s/\.\///g"`;
 		f=$DIR"/"$n;
 		count=` expr $count + 1`;
-		echo "scp -P 3600 -rp $f $ROMOTE/$n";
-		scp -P 3600 -rp $f "$ROMOTE/$n";
+		echo "scp -P $PORT -rp $f $ROMOTE/$n";
+		scp -P $PORT -rp $f "$ROMOTE/$n";
 	done
 else
 	f=$DIR"/*";
 	count="all";
 	echo "upload all file";
-	scp -P 3600 -rp $f "$ROMOTE"
+	scp -P $PORT -rp $f "$ROMOTE"
 fi
 touch $TIMESTEMP;
 
 echo "upload file :"$count;
-#sleep 1;
+sleep 1;
 #read;
--- VIMPROJ/majong.vim
+++ VIMPROJ/majong.vim
@@ -11,8 +11,10 @@ function! InitWorkSpace()
 	:set rnu
     ":e makefile
 	let g:SSHSendDir="/"
-	"let g:SSHSendRemot="LawrenceChi@192.168.100.144:/usr/server/Mahjong.gb/Complie/Mahjong/"
-	let g:SSHSendRemot="chiyl@192.168.1.108:/usr/server/Mahjong.gb/Complie/Mahjong/"
+	"let g:SSHSendRemote="LawrenceChi@192.168.100.144:/usr/server/Mahjong.gb/Complie/Mahjong/"
+	"let g:SSHPortRemote=3600
+	let g:SSHIPRemote="chiyl@192.168.1.108:/usr/server/Mahjong.gb/Complie/Mahjong/"
+	let g:SSHPortRemote=22
 endfunction
 
 call Main("G:/boyaa/MahjongTwServer")
