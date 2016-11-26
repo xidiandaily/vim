f62df2dc50eb505ed52d042d6f3b2c9a6146cfb4
chiyl <xidiandaily@126.com>
2016-11-26 13:48:05 +0800

    增加直接命令行启动项目；增加命令行直接改动项目

--- VIMPROJ/PROJ/launcher.sh
+++ VIMPROJ/PROJ/launcher.sh
@@ -7,6 +7,7 @@
 export __PRIORITY_FILE=".prio.launcher.chiyl";
 export __USER_INPUT_LETTER="";
 export __USER_CHOICE_PROJ="";
+export __USER_PROJ_NEWNAME="";
 export __RED='\033[0;31m'
 export __NC='\033[0m' # No Color
 export __VERSION="1.0";
@@ -14,7 +15,7 @@ export __CHOICE_TIPS="";
 export __SUPPORT_TYPES=(cpp php empty);
 export __OSTYPE=$OSTYPE;
 export __VIMPROJ=$VIMPROJ;
-declare -a projectlist;
+declare -a __PROJLIST;
 
 Init(){
     [[ $__OSTYPE = "cygwin" ]] && export __VIMPROJ=${VIMPROJ//\\/\/};
@@ -26,7 +27,7 @@ has_in_projectlist()
 { 
     local c=${1};
     local i;
-    for i in ${projectlist[@]}; do [[ $i = $c ]] && return 1;
+    for i in ${__PROJLIST[@]}; do [[ $i = $c ]] && return 1;
     done;
 
     return 0;
@@ -44,13 +45,13 @@ print_arr(){
 
 Open_prj(){
     local p=${1};
-    set -x;
+    local reg='.*darwin.*';
     [[ ! -f "$__VIMPROJ/$p.vim" ]] && echo -e "${__RED}$p not found ${__NC}" && exit;
     echo "$p" >> $__PRIORITY_FILE;
     if [[ $__OSTYPE = "cygwin" ]];then
         run env HOME=/cygdrive/c/Users/lawrencechi/ VIMPROJ=D:/Vim/VIMPROJ CYGWINPATH=C:/cygwin64/bin gvim -wait -S "$__VIMPROJ/$p.vim" &
         sleep 10;
-    elif [[ $__OSTYPE =~ "*darwin*" ]];then
+    elif [[ $__OSTYPE =~ $reg ]];then
         /Applications/MacVim.app/Contents/MacOS/Vim -g -S "$__VIMPROJ/$p.vim";
     else
         echo "$__OSTYPE not support";
@@ -60,16 +61,10 @@ Open_prj(){
 
 Rename_prj(){
     local old_projname=${1};
-    local proj_name="";
+    local proj_name=${2};
+    [[ -z $old_projname ]] || [[ -z $proj_name ]] && echo -e "old_projname:$old_projname or proj_name:$proj_name EMPTY" && exit;
     [[ ! -f "$__VIMPROJ/$old_projname.vim" ]] && echo -e "${__RED} ${__VIMPROJ}/$old_projname.vim NOT FOUND!!${__NC}" && exit;
-
-    while true;do
-        read -p "NEW PROJ NAME:" proj_name;
-        [[ -z "$proj_name" ]] && continue;
-        echo "$proj_name" | grep -e '|' -e '/' -e ' ' -e '*' -e '\\' && echo -e "${__RED}format error${__NC}" && continue;
-        [[ -f "$__VIMPROJ/$proj_name.vim" ]] && "PROJ Exists" && continue;
-        break;
-    done;
+    [[ -f "$__VIMPROJ/$proj_name.vim" ]] && echo -e "${__RED} ${__VIMPROJ}/$proj_name.vim EXIT!!${__NC}" && exit;
 
     mv "${__VIMPROJ}/$old_projname.vim" "${__VIMPROJ}/$proj_name.vim";
     cat $__PRIORITY_FILE | grep -v -w "$old_projname" > $__PRIORITY_FILE;
@@ -146,7 +141,7 @@ Select_prj()
     done;
 }
 
-Main_Choice_proj()
+List_prj()
 {
     if [[ -f $__PRIORITY_FILE ]]; then
         oldifs="$IFS";
@@ -157,11 +152,11 @@ Main_Choice_proj()
         for((n=${#pri[@]}-1;n>=0;n--));do i=${pri[$n]};
             has_in_projectlist $i;
             [[ $? -eq 1 ]] && continue;
-            projectlist+=($i);
+            __PROJLIST+=($i);
         done;
 
         rm -f $__PRIORITY_FILE;
-        for((n=${#projectlist[@]}-1;n>=0;n--));do i=${projectlist[$n]};
+        for((n=${#__PROJLIST[@]}-1;n>=0;n--));do i=${__PROJLIST[$n]};
             echo $i>>$__PRIORITY_FILE;
         done;
     fi
@@ -171,21 +166,25 @@ Main_Choice_proj()
         [[ $i = "template" ]] && continue;
         has_in_projectlist $i;
         [[ $? -eq 1 ]] && continue;
-        projectlist+=($i);
+        __PROJLIST+=($i);
     done;
+}
 
+Main_Choice_proj()
+{
+    List_prj;
     # 选择并启动对应的项目
     while true;do
         local -a my_prjlist=();
         if [[ ${#__USER_INPUT_LETTER} -ne 0 ]];then
-            for i in ${projectlist[@]};do
+            for i in ${__PROJLIST[@]};do
                 echo "$i" | grep -i "$__USER_INPUT_LETTER" >/dev/null 2>&1 ;
                 [[ "$?" -eq 1 ]] && continue;
                 my_prjlist+=($i);
             done;
             my_prjlist+=("ALL");
         else
-            my_prjlist=${projectlist[@]};
+            my_prjlist=${__PROJLIST[@]};
         fi
         my_prjlist+=("EXIT");
         Select_prj ${my_prjlist[@]};
@@ -233,36 +232,67 @@ Main_Add_proj(){
     esac
 }
 
+Main_Rename(){
+    local proj_name="";
+    while true;do
+        read -p "NEW PROJ NAME:" proj_name;
+        [[ -z "$proj_name" ]] && continue;
+        echo "$proj_name" | grep -e '|' -e '/' -e ' ' -e '*' -e '\\' && echo -e "${__RED}format error${__NC}" && continue;
+        [[ -f "$__VIMPROJ/$proj_name.vim" ]] && "PROJ Exists" && continue;
+        break;
+    done;
+
+    export __USER_PROJ_NEWNAME=$proj_name;
+}
+
+Main_Showproj(){
+    List_prj;
+    for i in ${__PROJLIST[@]};do
+        echo $i;
+    done
+}
+
 Main_help(){
     echo "${0#.*/} version:$__VERSION author by lawrencechi";
     echo "email: codeforfuture{AT}126.com";
     echo "";
     echo "manual:";
-    echo "$0       display all project and choice ";
-    echo "$0 -a    create proj";
-    echo "$0 -r    rename proj";
-    echo "$0 -d    delete proj";
-    echo "$0 -v    display version";
-    echo "$0 -h    display help";
+    echo "$0                                  display all project and choice ";
+    echo "$0 -r                               display all project and choice ";
+    echo "$0 -l                               list all project";
+    echo "$0 -a                               create proj";
+    echo "$0 -m                               rename proj";
+    echo "$0 -m [projname] [newprojectname]   rename proj";
+    echo "$0 -d                               delete proj";
+    echo "$0 -v                               display version";
+    echo "$0 -h                               display help";
 }
 
 Init;
-if [[ $# -eq 0 ]];then
-    export __CHOICE_TIPS="Open Project:";
-    Main_Choice_proj;
-    if [[ $? -eq 0 ]];then
+if [[ $# -eq 0 ]] || [[ ${1} = "-r" ]];then
+    export __USER_CHOICE_PROJ=${2};
+    if [[ -z $__USER_CHOICE_PROJ ]];then
+        export __CHOICE_TIPS="Open Project:";
+        Main_Choice_proj;
+    fi
+    if ! [[ -z $__USER_CHOICE_PROJ ]];then
         Open_prj "$__USER_CHOICE_PROJ";
     fi
 elif [[ ${1} = "-a" ]];then
     echo "Create New Proj";
     Main_Add_proj;
-elif [[ ${1} = "-r" ]];then
-    echo "Rename Proj";
-    export __CHOICE_TIPS="Rename Project:";
-    Main_Choice_proj;
-    if [[ $? -eq 0 ]];then
-        Rename_prj "$__USER_CHOICE_PROJ";
+elif [[ ${1} = "-m" ]];then
+    export __USER_CHOICE_PROJ=${2};
+    export __USER_PROJ_NEWNAME=${3};
+    if [[ -z $__USER_PROJ_NEWNAME ]] || [[ -z $__USER_CHOICE_PROJ ]];then
+        echo "Rename Proj";
+        export __CHOICE_TIPS="Rename Project:";
+        Main_Choice_proj;
+        if [[ $? -eq 0 ]];then
+            Main_Rename;
+        fi
     fi
+    Rename_prj "$__USER_CHOICE_PROJ" "$__USER_PROJ_NEWNAME";
 elif [[ ${1} = "-d" ]];then
     export __CHOICE_TIPS="Delete Project:";
     echo "Choice Delete Proj";
@@ -270,6 +300,8 @@ elif [[ ${1} = "-d" ]];then
     if [[ $? -eq 0 ]];then
         Del_prj "$__USER_CHOICE_PROJ";
     fi
+elif [[ ${1} = "-l" ]];then
+    Main_Showproj;
 elif [[ ${1} = "-v" ]];then
     echo "version:"$__VERSION;
 else
