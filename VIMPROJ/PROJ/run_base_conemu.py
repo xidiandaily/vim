bd40388316ba8829750929b80af3d9987f2a8ca2
lawrencechi <codeforfuture@126.com>
2023-10-09 21:26:36 +0800

    将launch.py 中的路径改成从配置读取;
    Signed-off-by: lawrencechi <codeforfuture@126.com>

--- VIMPROJ/PROJ/run_base_conemu.py
+++ VIMPROJ/PROJ/run_base_conemu.py
@@ -1,5 +1,9 @@
 import subprocess
+import configparser
 
-conemu_path = "C:\\Program Files\\ConEmu\\ConEmu64.exe"  # 替换为实际的 ConEmu.exe 路径
-your_script_exe_path = "G:\\CodeBase.github\\vim\\VIMPROJ\\PROJ\\launch.exe"  # 替换为实际的 your_script.exe 路径
+config=configparser.ConfigParser()
+config.read('config.ini')
+
+conemu_path = config.get('global','conEmu_path')  # 替换为实际的 ConEmu.exe 路径
+your_script_exe_path = config.get('global','vimproj_path')  # 替换为实际的 your_script.exe 路径
 subprocess.run([conemu_path,"/single","/run", f"{your_script_exe_path} -new_console:n"])
